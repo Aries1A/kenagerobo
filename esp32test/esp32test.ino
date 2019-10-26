55a24ef67c720045626a6d5c88197787345f63c3
Aries1A <kaiso.mar26.littleswitch@gmail.com>
2019-10-26 23:41:26 +0900

    add esp32test

--- esp32test/esp32test.ino
+++ esp32test/esp32test.ino
@@ -1,8 +1,71 @@
-void setup() {
+/*--------------------------------------Client--------------------------------------*/
+#include<WiFi.h>
+const char *ssid="elecom2g-c08850"; //サーバーのSSID
+const char *pass="3A7344CE7169FD2122B9F397AA3EB79F212E5E0741AEF3BB5A5E6BC652F15C0F"; //サーバーのパスワード
+static WiFiClient client; //WiFiClient型でclientと宣言
+#define TIME 10
+#define uS 1000*1000
+/*-------------サーバーとWiFi接続-------------*/
+bool server_connect()
+{
+  int try_count = 0;
+  WiFi.disconnect();
+  WiFi.begin(ssid, pass); //サーバーに接続
+  delay(1000);
+
+  while (WiFi.status() != WL_CONNECTED&&try_count<10)
+  {
+    try_count++;
+    Serial.print(".");
+    delay(1000);
+  }
+  if(WiFi.status()==WL_CONNECTED)
+  {
+    Serial.println("WiFi Connected");
+    IPAddress ip(192, 168, 0, 9); //サーバーのIPアドレス
+    client.connect(ip, 80); //IPアドレスとポート番号を指定して接続
+    return true;
+  }
+  else if(try_count == 10)
+  {
+    Serial.println("WiFi Connection:False");
+    WiFi.disconnect();
+    delay(100);
+    return false;
+  }
+}
+void connect_try()
+{
+  Serial.println("●WiFi Connnect Start");
+  int try_count = 0;
+  while(server_connect()!=true&&try_count<5)
+  {
+    try_count++;
+    delay(1000);
+  }
+  if(try_count==5)
+  {
+    Serial.println("False");
+  }
+}
+void setup()
+{
   Serial.begin(115200);
+  Serial.println("Program Start");
+  esp_sleep_enable_timer_wakeup(TIME * uS);
+  connect_try();
 }
 
-void loop() {
-  Serial.println("Hello World");
-  delay(1000);
+void loop()
+{
+  //サーバーに接続されたか確認
+  if(client.connected()==true)
+  {
+    Serial.println("Massage Send");
+    client.println("Hello");
+    client.stop();
+    WiFi.disconnect();
+    esp_deep_sleep_start();
+  }
+
 }
--- pyscript/simplesetver.py
+++ pyscript/simplesetver.py
@@ -1,19 +1,25 @@
 import sys
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
 class Client(BaseHTTPRequestHandler):
 
 	def do_POST(self):
+		# 受け取ったデータ
 		content_len = int(self.headers.get('content-length'))
-		body = b'OK'
+		requestBody = self.rfile.read(content_len).decode('ascii')
+		print(requestBody)
+		requestJson = json.loads(requestBody)
+		print(requestJson)
+		print("length = " + str(content_len))
+		body = b'OK' # 返す文字列
 		self.send_response(200)
-		self.send_header('Content-type', 'text/plain')
+		self.send_header('Content-type', 'text/plain') # 返す文字列によって変える
 		self.send_header('Content-length', len(body))
 		self.end_headers()
 		self.wfile.write(body)
-		requestBody = self.rfile.read(content_len).decode('ascii')
-		print("length = " + str(content_len))
-		print("body = " + requestBody)
 
-server = HTTPServer(('', 80), Client)
+
+
+server = HTTPServer(('', 86), Client)
 server.serve_forever()
